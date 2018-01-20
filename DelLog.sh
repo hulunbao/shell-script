71a84de0daa122e34074b7869c1b5d0ce63102cd
hulunbao <36943905+hulunbao@users.noreply.github.com>
2018-01-20 14:55:24 +0800

    delete log

--- DelLog.sh
+++ DelLog.sh
@@ -1,13 +1,14 @@
-#!/bin/bash 
+#!/bin/bash
 # 清除日志脚本
+
 
 LOG_DIR=/var/log
 ROOT_ID=0  # $ROOT_UID为0的用户 ，即root用户
 
 # 判断当前用户是否为root用户，不是则给出相应的提示
 
-if [ "$UID" -ne "$ROOT_ID" ] 
-  then 
+if [ "$UID" -ne "$ROOT_ID" ]
+  then
     echo "Must be root to run this script."
     exit 1
 fi
